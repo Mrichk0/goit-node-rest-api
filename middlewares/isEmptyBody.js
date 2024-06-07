38113f363fcf6a5051ff97bc1510456152a1743e
Mrichko Bohdan <zaq4769@gmail.com>
2024-06-07 10:56:19 +0300

    change error messege

--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -2,7 +2,7 @@ import HttpError from "../helpers/HttpError.js";
 
 const isEmptyBody = (req, res, next) => {
   if (Object.keys(req.body).length === 0) {
-    return next(HttpError(400, "Body is empty"));
+    return next(HttpError(400, "Body must have at least one field"));
   }
   next();
 };
