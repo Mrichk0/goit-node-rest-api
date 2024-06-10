c014676428899595a8c052fd05fd81dfa31869c1
Mrichko Bohdan <zaq4769@gmail.com>
2024-06-10 17:33:57 +0300

    add dotenv

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
